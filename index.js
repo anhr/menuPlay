0f1435818468b50fe8b932c281874142d99912d9
Andrej <anhr@mail.ru>
2019-10-14 04:15:02 +0700

    current version

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 ﻿/**
- * My dropdown menu for canvas in my version of [dat.gui](https://github.com/anhr/dat.gui) for playing of 3D obects in my projects.
+ * My dropdown menu for canvas in my version of [dat.gui](https://github.com/anhr/dat.gui) for playing of 3D objects in my projects.
  * 
  * @author Andrej Hristoliubov https://anhr.github.io/AboutMe/
  *
- * Copyright 2011 Data Arts Team, Google Creative Lab
+ * @copyright 2011 Data Arts Team, Google Creative Lab
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * @license under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
@@ -31,72 +31,87 @@ import { create as dropdownMenuCreate } from '../DropdownMenu/index.js';
  */
 
 /**
- * dropdown menu for canvas for playing of 3D obects
- * @param {HTMLElement|String} elContainer if the HTMLElement is a container element for canvas. If the String is id of a container element for canvas.
+ * @callback onFullScreenToggle
+ */
+
+/**
+ * dropdown menu for canvas for playing of 3D objects
+ * @param {HTMLElement|String} elContainer if the HTMLElement, is a container element for canvas. If the String, is id of a container element for canvas.
  * @param {Object} [options] optional options.
  * @param {Object} [options.stereoEffect] new THREE.StereoEffect(...) https://github.com/anhr/three.js/blob/dev/examples/js/effects/StereoEffect.js
- * @param {Object} [options.playController] playController https://github.com/anhr/controllerPlay - my custom controller in my version of dat.gui(https://github.com/anhr/dat.gui) for playing of 3D obects in my projects.
- * @param {onFullScreen} [options.onFullScreen] user toggled fullscreen mode of the canvas.
+ * @param {Object} [options.player] new Player(...) playing of 3D ojbects in my projects. Defauilt is undefined
+ * See myThreejs\player.js for details.
+ * @param {onFullScreen} [options.onFullScreen] fullscreen mode of the canvas.
+ * @param {onFullScreenToggle} [options.onFullScreenToggle] user toggled fullscreen mode of the canvas.
  */
 export function create( elContainer, options ) {
 
 	var elCanvas = elContainer.querySelector( 'canvas' );
 
 	options = options || {};
-	var playController = options.playController,
-		stereoEffect = options.stereoEffect.stereoEffect,
+
+	var stereoEffect, spatialMultiplexsIndexs;
+	if ( options.stereoEffect !== undefined ) {
+
+		stereoEffect = options.stereoEffect.stereoEffect;
 		spatialMultiplexsIndexs = options.stereoEffect.spatialMultiplexsIndexs;
 
-	var menu = [];
+	}
+
+	var menu = [], menuItemStereoEffect;
 
 	//stereoEffect
-	if ( options.stereoEffect )
-		menu.push( {
+	if ( options.stereoEffect ) {
+
+		menuItemStereoEffect = {
 
 			name: '⚭',
-			title: 'Stereo effects',
+			title: lang.stereoEffects,//'Stereo effects',
 			id: 'menuButtonStereoEffects',
 			drop: 'up',
 			items: [
 
 				{
-					name: 'Mono',
+					name: lang.mono,//'Mono',
 					radio: true,
 					checked: true,
+					spatialMultiplex: spatialMultiplexsIndexs.Mono,
 					onclick: function ( event ) {
 
 						if ( stereoEffect.setSpatialMultiplex !== undefined )
 							stereoEffect.setSpatialMultiplex( spatialMultiplexsIndexs.Mono );
 						else stereoEffect.options.spatialMultiplex = spatialMultiplexsIndexs.Mono;
-//						setFullScreenButton( false );
+						//						setFullScreenButton( false );
 						if ( options.onFullScreen )
 							options.onFullScreen( false );
 
 					}
 				},
 				{
-					name: 'Side by side',
+					name: lang.sideBySide,//'Side by side',
 					radio: true,
+					spatialMultiplex: spatialMultiplexsIndexs.SbS,
 					onclick: function ( event ) {
 
 						if ( stereoEffect.setSpatialMultiplex !== undefined )
 							stereoEffect.setSpatialMultiplex( spatialMultiplexsIndexs.SbS );
 						else stereoEffect.options.spatialMultiplex = spatialMultiplexsIndexs.SbS;
-//						setFullScreenButton( true );
+						//						setFullScreenButton( true );
 						if ( options.onFullScreen )
 							options.onFullScreen( true );
 
 					}
 				},
 				{
-					name: 'Top and bottom',
+					name: lang.topAndBottom,//'Top and bottom',
 					radio: true,
+					spatialMultiplex: spatialMultiplexsIndexs.TaB,
 					onclick: function ( event ) {
 
 						if ( stereoEffect.setSpatialMultiplex !== undefined )
 							stereoEffect.setSpatialMultiplex( spatialMultiplexsIndexs.TaB );
 						else stereoEffect.options.spatialMultiplex = spatialMultiplexsIndexs.TaB;
-//						setFullScreenButton( true );
+						//						setFullScreenButton( true );
 						if ( options.onFullScreen )
 							options.onFullScreen( true );
 
@@ -105,9 +120,12 @@ export function create( elContainer, options ) {
 
 			],
 
-		} );
+		}
+		menu.push( menuItemStereoEffect );
 
-	if ( options.playController !== undefined ) {
+	}
+
+	if ( options.player !== undefined ) {
 
 		//Previous button
 		menu.push( {
@@ -116,11 +134,13 @@ export function create( elContainer, options ) {
 			title: lang.prevSymbolTitle,
 			onclick: function ( event ) {
 
-				playController.prev();
+//				playController.prev();
+				options.player.prev();
 
 			}
 
 		} );
+
 		//Play button
 		menu.push( {
 
@@ -129,24 +149,28 @@ export function create( elContainer, options ) {
 			id: "menuButtonPlay",
 			onclick: function ( event ) {
 
-				playController.play();
+				options.player.play3DObject();
+//				playController.play();
 
 			}
 
 		} );
+
 		//Repeat button
 		menu.push( {
 
 			name: lang.repeat,
-			title: lang.repeatOn,
+			title: options.player.getOptions().repeat ? lang.repeatOff : lang.repeatOn,
 			id: "menuButtonRepeat",
 			onclick: function ( event ) {
 
-				playController.repeat();
+//				playController.repeat();
+				options.player.repeat();
 
 			}
 
 		} );
+
 		//Next button
 		menu.push( {
 
@@ -154,7 +178,7 @@ export function create( elContainer, options ) {
 			title: lang.nextSymbolTitle,
 			onclick: function ( event ) {
 
-				playController.next();
+				options.player.next();
 
 			}
 
@@ -185,14 +209,14 @@ export function create( elContainer, options ) {
 	} );
 
 	//Play slider
-	if ( options.playController !== undefined ) {
+	if ( options.player !== undefined ) {
 
-		var group = options.playController.getGroup();
+//		var group = options.playController.getGroup();
 		menu.push( {
 
-			name: '<input type="range" min="0" max="' + ( group.children.length - 1 ) + '" value="0" class="slider" id="sliderPosition">',
+			name: '<input type="range" min="0" max="' + ( options.player.getOptions().marks - 1 ) + '" value="0" class="slider" id="sliderPosition">',
 			style: 'float: right;',
-			title: sliderTitle + 0,
+//			title: sliderTitle + 0,
 
 		} );
 
@@ -205,14 +229,21 @@ export function create( elContainer, options ) {
 		decorations: 'Transparent',
 
 	} );
-	var elSlider = elMenu.querySelector( '#sliderPosition' );
-	if ( elSlider !== null )
+	elSlider = elMenu.querySelector( '#sliderPosition' );
+	if ( elSlider !== null ) {
+
 		elSlider.onchange = function ( event ) {
 
-			//console.log( 'position: ' + elSlider.value );
-			playController.selectObject3D( elSlider.value );
+			options.player.selectScene( elSlider.value );
 
 		};
+		elSlider.oninput = function ( event ) {
+
+			options.player.selectScene( elSlider.value );
+
+		};
+
+	}
 
 	this.setFullScreenButton = function( fullScreen ) {
 
@@ -222,12 +253,12 @@ export function create( elContainer, options ) {
 		if ( fullScreen ) {
 
 			elMenuButtonFullScreen.innerHTML = '⤦';
-			elMenuButtonFullScreen.title = 'Non Full Screen';
+			elMenuButtonFullScreen.title = lang.nonFullScreen;
 
 		} else {
 
 			elMenuButtonFullScreen.innerHTML = '⤢';
-			elMenuButtonFullScreen.title = 'Full Screen';
+			elMenuButtonFullScreen.title = lang.fullScreen;
 
 		}
 		return true;
@@ -278,12 +309,78 @@ export function create( elContainer, options ) {
 		}
 
 	}
-	this.setIndex = function( index ) {
+	this.setIndex = function ( index, title ) {
 
 		elSlider.value = index;
-		elSlider.title = sliderTitle + index;
+//		elSlider.title = sliderTitle + t;
+		elSlider.title = title;
 
 	}
-	var elMenu, elSlider, sliderTitle = 'current position of the playing is ';
+	this.onRenamePlayButtons = function ( playing ) {
+
+		var name, title;
+		if ( playing ) {
+
+			name = lang.pause;
+			title = lang.pauseTitle;
+
+		} else {
+
+			name = lang.playSymbol;
+			title = lang.playTitle;
+
+		}
+		var elMenuButtonPlay = elMenu.querySelector( '#menuButtonPlay' );
+		elMenuButtonPlay.innerHTML = name;
+		elMenuButtonPlay.title = title;
+//		_renamePlayButtons( name, title, true );
+
+	}
+	this.onChangeRepeat = function () {
+
+		var elMenuButtonRepeat = elMenu.querySelector( '#menuButtonRepeat' );
+		elMenuButtonRepeat.title = options.player.getOptions().repeat ? lang.repeatOff : lang.repeatOn;
+
+	}
+	this.onChangeScale = function ( scale ) {
+
+		if ( options.player === undefined )
+			return;
+		var optionsPlayer = options.player.getOptions();
+		optionsPlayer.marks = scale.marks;
+		optionsPlayer.min = scale.min;
+		optionsPlayer.max = scale.max;
+		elSlider.max = optionsPlayer.marks - 1;
+
+	}
+	this.setSpatialMultiplexs = function ( mode ) {
+
+		menuItemStereoEffect.items.forEach( function ( item ) {
+
+			if ( item.spatialMultiplex === mode ) {
+
+				if ( !item.checked ) {
+
+//					item.checked = true;
+					item.elName.onclick( { target: item.elName });
+
+				}
+
+			}// else item.checked = false;
+
+		} );
+
+	}
+	this.setPlayer = function ( player ) {
+
+		options.player = player;
+		player.controllers.push( this );
+		elSlider.value = 0;
+
+	}
+	if ( options.player !== undefined )
+		options.player.pushController( this );
+//		options.player.controllers.push( this );
+	var elMenu, elSlider;//, sliderTitle = lang.animateSceneId;//'Animate scene id ';
 
 }
